5fc0bd71974c3ff947677be0f6d2935981b63e76
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-22 23:34:29 +0530

    Create 5. Count Characters.java

---
+++ String-Interview Programs/5. Count Characters.java
@@ -0,0 +1,12 @@
+5. Count Characters
+public class CountCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        System.out.println(str.length());
+    }
+}
+Output
+4
