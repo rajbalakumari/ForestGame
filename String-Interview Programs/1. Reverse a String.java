5745977d53433ef8ae735b351d6ec7ea3bf24d23
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-22 23:33:07 +0530

    Create 1. Reverse a String.java

---
+++ String-Interview Programs/1. Reverse a String.java
@@ -0,0 +1,19 @@
+1. Reverse a String
+Program
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "Java";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println("Original String : " + str);
+        System.out.println("Reversed String : " + rev);
+    }
+}
+Output
+Original String : Java
+Reversed String : avaJ
