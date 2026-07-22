70bc19ac6aa80306517eeba2bf6e2a73b2b16ba4
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-22 23:34:09 +0530

    Create 4. Count Words.java

---
+++ String-Interview Programs/4. Count Words.java
@@ -0,0 +1,14 @@
+4. Count Words
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        String str = "Java is Easy";
+
+        String arr[] = str.split(" ");
+
+        System.out.println("Words = " + arr.length);
+    }
+}
+Output
+Words = 3
