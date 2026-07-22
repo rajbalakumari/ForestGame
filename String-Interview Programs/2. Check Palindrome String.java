04340744e68f3c7b269c30a530d03740d3480719
Raj Bala Kumari <138221713+rajbalakumari@users.noreply.github.com>
2026-07-22 23:33:30 +0530

    Create 2. Check Palindrome String.java

---
+++ String-Interview Programs/2. Check Palindrome String.java
@@ -0,0 +1,20 @@
+2. Check Palindrome String
+Program
+public class PalindromeString {
+    public static void main(String[] args) {
+
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        if (str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
+Output
+Palindrome
